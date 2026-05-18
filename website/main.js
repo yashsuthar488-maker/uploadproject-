c8fe12dcfc5257e41ce3ec2053d94ccb8d76ded6
yashsuthar488-maker <yashsuthar488@gmail.com>
2026-05-18 10:55:42 +0530

    Add files via upload

---
+++ website/main.js
@@ -0,0 +1,10 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    });
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    });
